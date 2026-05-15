ea8a8aa52a8e19970aef994d05f33e1346f2c7e6
AhmedYeasin <ahmetchatgami@gmail.com>
2026-05-16 00:03:06 +0600

    payment success functionalities

--- src/pages/Dashboard/Payment/PaymentSuccess.jsx
+++ src/pages/Dashboard/Payment/PaymentSuccess.jsx
@@ -1,14 +1,29 @@
-import React from "react";
-import { Link } from "react-router";
+import React, { useEffect } from "react";
+import { Link, useSearchParams } from "react-router";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
-const PaymentSuccess=()=>{
-return (
-    <div>
-        <h2>Payment Successfull</h2>
-        <Link to='/dashboard/my-parcels' className="btn btn-primary text-black">View Parcels</Link>| OR |
-        <Link to='/' className="btn btn-primary text-black">Back to Home</Link>
-    </div>
-);
+const PaymentSuccess = () => {
+
+    const [searchParams] = useSearchParams();
+    const sessionId = searchParams.get('session_id');
+    const axiosSecure = useAxiosSecure();
+
+    useEffect(()=>{
+        if(sessionId){
+            axiosSecure.patch(`/payment-success?sessionId=${sessionId}`)
+            .then(res=>{
+                console.log(res.data)
+            })
+        }
+    }, [sessionId, axiosSecure])
+
+    return (
+        <div>
+            <h2>Payment Successfull</h2>
+            <Link to='/dashboard/my-parcels' className="btn btn-primary text-black">View Parcels</Link>| OR |
+            <Link to='/' className="btn btn-primary text-black">Back to Home</Link>
+        </div>
+    );
 };
 
 export default PaymentSuccess;
